aab4dbeff0f2fa89fabdcd2969d5993d8df0fc06
LucDSilva <lucas.silva00@outlook.ie>
2020-01-14 15:54:22 +0000

    added additional methods to notes

--- app.js
+++ app.js
@@ -2,9 +2,41 @@ const chalk = require('chalk');
 const yargs = require('yargs');
 const getNotes = require('./notes');
 
-// Customizing yargs
+// Customizing yargs version
+yargs.version('1.1.0');
 
-// Listing Command
+// Create Add Command
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  builder: {
+    title: {
+      describe: "Note Title",
+      demandOption: true,
+      type: 'string'
+    },
+    body: {
+      describe: 'Notes body',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler: function(argv){
+    console.log('Title: ' + argv.title);
+    console.log('Body: ' + argv.body);
+  }
+});
+
+// Create Remove Command
+yargs.command({
+  command: "remove",
+  describe: "Removes all my notes",
+  handler: function(){
+    console.log("Removing Notes...")
+  }
+});
+
+// Create Listing Command
 yargs.command({
   command: "list",
   describe: "lists all my notes",
@@ -13,7 +45,7 @@ yargs.command({
   }
 });
 
-// Read Command
+// Create Read Command
 yargs.command({
   command: "read",
   describe: "Reads the notes",
@@ -22,4 +54,4 @@ yargs.command({
   }
 })
 
-console.log(yargs.argv)
+yargs.parse();
