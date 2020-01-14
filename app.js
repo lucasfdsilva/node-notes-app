4ebb4992949f8d81d94c66b7db379368f3186a75
LucDSilva <lucas.silva00@outlook.ie>
2020-01-14 06:45:09 +0000

    added chalk, nodemon and configured yargs for command line args manipulation

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
-const validator = require('validator');
+const chalk = require('chalk');
+const yargs = require('yargs');
+const getNotes = require('./notes');
 
-console.log(validator.isEmail('andrew@example.com'));
-console.log(validator.isURL('https//mead.io'));
+// Customizing yargs
+
+// Listing Command
+yargs.command({
+  command: "list",
+  describe: "lists all my notes",
+  handler: function(){
+    console.log("Listing Notes...")
+  }
+});
+
+// Read Command
+yargs.command({
+  command: "read",
+  describe: "Reads the notes",
+  handler: function(){
+    console.log("Reading Notes...")
+  }
+})
+
+console.log(yargs.argv)
